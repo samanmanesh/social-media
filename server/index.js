0d02c089a9583a30738f04f340cf07872a1b7321
Saman Manesh <samanmanesh7@gmail.com>
2022-01-17 00:08:17 -0500

    conected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8800;
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 dotenv.config();
 
-app.listen(port,()=>{
-  console.log("backend server is running on port 8800");
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to mongoDB");
+  }
+);
+
+app.listen(port, () => {
+  console.log(
+    "backend server is running on port 8800"
+  );
 });
