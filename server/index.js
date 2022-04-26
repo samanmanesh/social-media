9b0a5cf99bfa295d1be24a99af6a0adf51d1bbd4
Saman Manesh <samanmanesh7@gmail.com>
2022-04-26 13:26:15 -0400

    added auth route/ about to add controller folder

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
+import authRoute from "./routes/auth.js";
 
 const app = express();
 const port = process.env.port || 8800;
@@ -33,6 +34,7 @@ app.use(morgan("common"));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello Social Media");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-  res.send("Hey this is Users route");
+  res.send("Hey this is auth route");
 });
 
 export default router;
