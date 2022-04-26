d03987636424428d7b38633bbdca91766322c882
Saman Manesh <samanmanesh7@gmail.com>
2022-04-26 15:35:49 -0400

    try the register and it works

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,25 @@
 import express from "express";
+import User from '../models/user.js';
 const router = express.Router();
 
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  // const user = new User(req.body); for client side request
+  const user = new User ({
+    userName: "Sam",
+    email: "Sam@gmail.com",
+    password: "12345678",
+  })
+
+  try {
+    const newUser = await user.save();
+    res.send(newUser);
+  }
+  catch (err) {
+    res.send(err);
+  }
+});
 
 router.get("/", (req, res) => {
   res.send("Hey this is auth route");
