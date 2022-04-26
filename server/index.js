bbfafdb142afa49f7cd41a04ae3289b5923e3a0b
Saman Manesh <samanmanesh7@gmail.com>
2022-04-26 11:08:28 -0400

    changing the server module

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const app = express();
-const port = 8800;
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
+// const express = require("express");
+import express from "express";
+
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
+// const dotenv = require("dotenv");
+import dotenv from "dotenv";
+// const helmet = require("helmet");
+import helmet from "helmet";
+// const morgan = require("morgan");
+import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+const app = express();
+const port = 8800;
 dotenv.config();
 
 // mongoose.connect(
