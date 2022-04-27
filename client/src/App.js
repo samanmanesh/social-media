38c7c3e5787cec64884640b6d594101f08dcadec
Saman Manesh <samanmanesh7@gmail.com>
2022-04-27 19:04:56 -0400

    install tailwind css and removed unnessery stuffs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="h-screen bg-gray-800" >
+      <h1 class="text-white">Hello Saman</h1>
+
     </div>
   );
 }
