fc0757de062f807a4fce82070e6d101d60974034
Saman Manesh <samanmanesh7@gmail.com>
2022-04-27 16:09:58 -0400

    added deletePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,3 +27,18 @@ export const updatePost = async (req, res) => {
     return res.status(500).json(error);
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res
+        .status(200)
+        .json({ message: "Post deleted successfully" });
+    } else
+      return res.status(401).json("You are not authorized to delete this post");
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+  }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,36 +1,21 @@
 import express from "express";
-import { createPost, updatePost } from "../controllers/posts.js";
+import { createPost, updatePost, deletePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-
 //create a post
-router.post( '/', createPost );
+router.post("/", createPost);
 
 //update a post
-router.put( '/:id', updatePost );
+router.put("/:id", updatePost);
 //delete a post
+router.delete("/:id", deletePost);
 //like a post
-//get a post 
+//get a post
 //get timeline posts
 
-
-
-router.get('/', (req, res) => {
-  res.send('Hello from Posts')
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.get("/", (req, res) => {
+  res.send("Hello from Posts");
+});
 
 export default router;
