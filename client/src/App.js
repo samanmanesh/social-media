2fd82635a09911b6705b6898ba999849a2cacb37
Saman Manesh <samanmanesh7@gmail.com>
2022-04-28 21:31:36 -0400

    start working on components

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Home from './pages/home/Home';
 function App() {
   return (
-    <div class="h-screen bg-gray-800" >
-      <Home />
+    <div class=" h-screen " >
+      <Home  />
     </div>
   );
 }
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,6 +2,13 @@
 
 export default function TopBar() {
   return (
-    <div>TopBar</div>
+    <div class=" flex justify-between p-4">
+
+        <span class="  ">Xenophon</span>
+        
+        <input type="text" placeholder="Search" class="p-1 bg-neutral-800 rounded-md w-80 focus:outline-none focus:ring focus:border-blue-500"/>
+        <div>buttons section</div>
+
+    </div>
   )
 }
