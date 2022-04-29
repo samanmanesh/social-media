8fb720ef6282696b39fdee628c16a0cfa56784ce
Saman Manesh <samanmanesh7@gmail.com>
2022-04-28 21:56:31 -0400

    working in topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,14 +1,18 @@
-
+import Navbar from "../Navbar";
 
 export default function TopBar() {
   return (
     <div class=" flex justify-between p-4">
+      <span class=" font-MsMadi italic font-black font text-2xl ">
+        Xenophon
+      </span>
 
-        <span class="  ">Xenophon</span>
-        
-        <input type="text" placeholder="Search" class="p-1 bg-neutral-800 rounded-md w-80 focus:outline-none focus:ring focus:border-blue-500"/>
-        <div>buttons section</div>
-
+      <input
+        type="text"
+        placeholder="Search"
+        class=" p-1 bg-neutral-800 rounded w-80 focus:outline-none focus:ring focus:border-blue-500"
+      />
+      <Navbar />
     </div>
-  )
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      MsMadi: ['Ms Madi', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
