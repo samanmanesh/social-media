03433f6a9fceb0662a52e474d167e3a8ad7564e0
Saman Manesh <samanmanesh7@gmail.com>
2022-04-29 11:32:31 -0400

    check

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./navbar/Navbar";
-import Search from "./navbar/Search";
+import Search from "./search/Search";
 
 export default function Header() {
   return (
-    <div className="flex justify-around px-16 py-2 border-b border-black ">
-      <span className="font-freehand font-black text-3xl drop-shadow-2xl">
+    <div className="flex justify-around px-16 py-2 border-b border-black sticky top-0">
+      <span 
+      style={{
+        filter: 'drop-shadow(0px 5px 3px rgba(0,0,0,0.2))',
+      }}
+      className="font-freehand font-black text-3xl drop-shadow-lg">
         Xenophone
       </span>
       <Search />
