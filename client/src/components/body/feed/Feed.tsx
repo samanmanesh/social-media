9aab6222875912a3c6e3f30e15b3a0724d0b34c9
Saman Manesh <samanmanesh7@gmail.com>
2022-05-01 17:30:46 -0400

    added padding to body

--- client/src/components/body/feed/Feed.tsx
+++ client/src/components/body/feed/Feed.tsx
@@ -7,8 +7,8 @@ interface Props {} // extends React.HTMLAttributes<HTMLDivElement> {}
 export default function Feed({...props}: Props) {
   const arrOfPosts = Array(10).fill(0);
   return (
-    <div className="container max-w-6xl flex flex-col md:flex-row ">
-      <div className="space-y-5 w-full   ">
+    <div className="container max-w-6xl flex flex-col md:flex-row pt-7">
+      <div className="space-y-5 w-full">
         {arrOfPosts.map((p, index) => (
           <PostCard key={index} />
         ))}
