1c90696b6072f6e986238ead63ca87a0253a7bcd
Saman Manesh <samanmanesh7@gmail.com>
2022-05-03 14:32:17 -0400

    styled the ProfileHeader

--- client/src/components/profile/profileheader/ProfileHeader.tsx
+++ client/src/components/profile/profileheader/ProfileHeader.tsx
@@ -1,31 +1,42 @@
-import React from "react";
-
 type Props = {};
 
 const ProfileHeader = (props: Props) => {
+  const username = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, velit?"
   return (
-    <div className="flex border-b border-gray-400">
-      <div>
-        {" "}
-        <img
-          src="./assets/people/courtney-hill-aixlh4umWQw-unsplash.jpg"
-          alt=""
-          className="rounded-full w-20 h-20 border border-gray-500 object-cover"
-        />
-      </div>
-      <div className="">
-        <div className="font-semibold">
-        <h1>User Name</h1>
-        <button>Edit Profile</button>
+    <div className="flex justify-center ">
+      <div className="max-w-4xl flex-1 flex border-b border-gray-400 py-8">
+        <div className="px-4 py-2 mr-4 md:mr-8 flex-shrink-0 grid place-items-center">
+          <img
+            src="./assets/people/courtney-hill-aixlh4umWQw-unsplash.jpg"
+            alt=""
+            className="rounded-full w-20 h-20 md:w-36 md:h-36 border border-gray-500 object-cover"
+          />
         </div>
-        <div >
-          <span> 19 posts</span> 
-          <span> 2,987 followers</span>
-          <span> 1,903 following</span>
-        </div>
-        <div className="">
-          <p> Name </p>
-          <div>bio what ever they want put here</div>
+        <div className="flex-1">
+          {/* Username Section */}
+          <div className="flex flex-col sm:flex-row justify-between items-start mb-4">
+            <h1 title={username} className="font-medium text-2xl overflow-hidden whitespace-nowrap text-ellipsis w-56 sm:w-64 cursor-default">
+              {username}
+            </h1>
+            <button className="w-full sm:w-max mt-2 sm:mt-0 sm:ml-2 border px-2 py-1 text-neutral-700 font-medium whitespace-nowrap rounded">
+              Edit Profile
+            </button>
+          </div>
+          <div className="hidden sm:block space-x-4 md:space-x-8">
+            <span>
+              <strong>19</strong> posts
+            </span>
+            <span>
+              <strong>2,987</strong> followers
+            </span>
+            <span>
+              <strong>1,903</strong> following
+            </span>
+          </div>
+          <div className="mt-2">
+            <p className="font-medium">Name</p>
+            <p>bio what ever they want put here</p>
+          </div>
         </div>
       </div>
     </div>
