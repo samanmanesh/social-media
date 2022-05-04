f2a5ed0a68f5e976f276fff0389beaa2f4ec3dea
Saman Manesh <samanmanesh7@gmail.com>
2022-05-04 12:11:52 -0400

    working on login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages";
 import ProfilePage from "./pages/profile/ProfilePage";
 import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />} >
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="profile" element={<ProfilePage />} />
           </Route>
--- client/src/pages/login/LoginPage.tsx
+++ client/src/pages/login/LoginPage.tsx
@@ -3,7 +3,22 @@ type Props = {}
 
 const LoginPage = (props: Props) => {
   return (
-    <div>LoginPage</div>
+    <div className="flex flex-col p-2 justify-center items-center bg-blue-200">
+      <h1 className="font-gemunu text-5xl p-4">Hilarion</h1>
+      <form action="" className="flex flex-col p-4 ">
+        <input type="text" placeholder="Username" className="border w-full h-full" />
+        <input type="password" placeholder="Password"  className=" border"/>
+        <button type="submit" className="border">Login</button>
+      </form>
+
+      <hr />
+
+      <h2>Or</h2>
+      <div className="flex flex-col">
+        <span>Don't have an account?</span>
+        <button>Sign up</button>
+        </div>
+    </div>
   )
 }
 
