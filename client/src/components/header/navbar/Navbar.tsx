4cd81fa4c726f355cc64c9cd2f72016f94e2d2a3
Saman Manesh <samanmanesh7@gmail.com>
2022-05-05 15:03:47 -0400

    added link to nav and home icon

--- client/src/components/header/navbar/Navbar.tsx
+++ client/src/components/header/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import {
 } from "@heroicons/react/outline";
 import { useState } from "react";
 import PostShareModal from './share/PostShareModal';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,9 +20,10 @@ export default function Navbar() {
 
   return (
     <div className="flex items-center space-x-5 mx-2">
-      <button>
+      <Link to="/">
         <HomeIcon className="w-7 h-7" />
-      </button>
+      </Link>
+      
       <button>
         <ChatAltIcon className="w-7 h-7" />
       </button>
