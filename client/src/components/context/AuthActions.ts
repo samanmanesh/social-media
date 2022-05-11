1764173bb6929df61887475bea3b98a427e738e1
Saman Manesh <samanmanesh7@gmail.com>
2022-05-11 10:42:50 -0400

    working on context and reducer module

---
+++ client/src/components/context/AuthActions.ts
@@ -0,0 +1,28 @@
+const 
+
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        isFetching: true,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        isFetching: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default AuthReducer;
