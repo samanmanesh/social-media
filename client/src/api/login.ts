ae67b065f6da159dd16f05ece18d1944d1c88b63
Saman Manesh <samanmanesh7@gmail.com>
2022-05-11 12:26:17 -0400

    debbuged

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -2,7 +2,12 @@ import { userCredentials } from "../components/context/AuthReducer";
 import { Actions } from "../components/context/AuthActions";
 import { instance } from "api";
 
-export const login = (userCredentials: userCredentials, dispatch ) => {
+interface Props {
+  userCredentials: userCredentials;
+  dispatch: React.Dispatch<Actions>;
+}
+
+export const login = ({ userCredentials, dispatch }: Props) => {
   dispatch({ type: "LOGIN_START" });
   return instance.post("auth/login", userCredentials);
 };
