2e95ada56afca320404341ca24d1ccc6ac612bb4
Saman Manesh <samanmanesh7@gmail.com>
2022-05-11 11:11:23 -0400

    debbuged

--- client/src/components/context/AuthContext.tsx
+++ client/src/components/context/AuthContext.tsx
@@ -5,9 +5,10 @@ export interface AuthState {
   user: User | null;
   isFetching: boolean;
   error: any;
+  dispatch?: any; // TODO: fix this type, I don't know whats its type
 }
 
-const INITIAL_STATE : AuthState = {
+const INITIAL_STATE: AuthState = {
   user: null,
   isFetching: false,
   error: null,
@@ -19,17 +20,16 @@ type Props = {
 
 export const AuthContext = createContext(INITIAL_STATE);
 
-export const AuthContextProvider = ({ children } : Props) => {
+export const AuthContextProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-
   return (
     <AuthContext.Provider
-      value{{
+      value={{
         user: state.user,
         isFetching: state.isFetching,
         error: state.error,
+        dispatch,
       }}
-      dispatch
     >
       {children}
     </AuthContext.Provider>
