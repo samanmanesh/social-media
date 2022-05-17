dc60484c065733d629b394874e5e4506ce3616f2
Saman Manesh <samanmanesh7@gmail.com>
2022-05-17 13:10:20 -0400

    adding profile menu drop down

--- client/src/layout/Navbar.tsx
+++ client/src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Menu } from '@headlessui/react'
+import { Menu } from "@headlessui/react";
 import { HomeIcon, PlusIcon } from "@heroicons/react/solid";
 import {
   HeartIcon,
@@ -6,16 +6,14 @@ import {
   UserGroupIcon,
 } from "@heroicons/react/outline";
 import { useState } from "react";
-import PostShareModal from './PostShareModal';
+import PostShareModal from "./PostShareModal";
 import { Link } from "react-router-dom";
-import { useAuth } from '../auth/utils';
+import { useAuth } from "../auth/utils";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const {user} =useAuth();
+  const { user } = useAuth();
   const username = user?.username;
-  
-  
 
   const openModal = () => {
     setIsOpen(true);
@@ -23,13 +21,12 @@ export default function Navbar() {
 
   const PF = process.env.REACT_APP_PUBLIC_FOLDER; // public folder path in env file for routing to work
 
-
   return (
     <div className="flex items-center space-x-5 mx-2">
       <Link to="/">
         <HomeIcon className="w-7 h-7" />
       </Link>
-      
+
       <button>
         <ChatAltIcon className="w-7 h-7" />
       </button>
@@ -49,34 +46,27 @@ export default function Navbar() {
       {/* use global state to go to user profile when its clicked */}
       {/* <Link to={`/profile/${user.id}`}  className="rounded-full  w-7 h-7" > */}
       <div className="rounded-full  w-7 h-7">
-      <Menu>
-      <Menu.Button>More</Menu.Button>
-      <Menu.Items>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'bg-blue-500'}`}
-              href="/account-settings"
-            >
-              Account settings
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'bg-blue-500'}`}
-              href="/account-settings"
-            >
-              Documentation
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item disabled>
-          <span className="opacity-75">Invite a friend (coming soon!)</span>
-        </Menu.Item>
-      </Menu.Items>
-    </Menu>
+        <Menu>
+          <Menu.Button>
+            <img
+              src={
+                user?.profilePicture
+                  ? PF + "/people/" + user.profilePicture
+                  : PF + "people/no-image-avatar2.png"
+              }
+              alt="profile"
+              className=" w-7 h-7 rounded-full object-cover border border-gray-400"
+            />
+          </Menu.Button>
+          <Menu.Items className="bg-white border rounded w-24 ">
+            <Menu.Item>
+              {({ active }) => <Link to={`/profile/${username}`}>Profile</Link>}
+            </Menu.Item>
+            <Menu.Item>{({ active }) => <button>Settings</button>}</Menu.Item>
+            <hr />
+            <Menu.Item>{({ active }) => <button>Log Out</button>}</Menu.Item>
+          </Menu.Items>
+        </Menu>
       </div>
       {/* <Link to={`/profile/${username}`} className="rounded-full  w-7 h-7" >
         {" "}
@@ -93,6 +83,3 @@ export default function Navbar() {
     </div>
   );
 }
-
-
-
