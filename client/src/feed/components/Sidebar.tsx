d4212326edab1fd419e918a4ba4c9d17e26a46dc
Saman Manesh <samanmanesh7@gmail.com>
2022-05-17 11:58:47 -0400

    fixed the Sidebar upper section

--- client/src/feed/components/Sidebar.tsx
+++ client/src/feed/components/Sidebar.tsx
@@ -1,18 +1,25 @@
 import { FriendSuggestion } from "./FriendSuggestion";
+import { useAuth } from "../../auth/utils";
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function Sidebar({ ...props }: Props) {
+  const { user } = useAuth();
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER; // public folder path in env file for routing to work
   return (
     <div className="h-96 sticky flex-shrink-0 top-20 p-6 hidden lg:block ml-8 w-80">
       <div className="flex space-x-4">
         <img
-          src="./assets/people/jan-kopriva-GUNKCYNYXHA-unsplash.jpg"
+          src={
+            user?.profilePicture
+              ? PF + "/people/" + user.profilePicture
+              : PF + "people/no-image-avatar2.png"
+          }
           alt="profile"
           className="rounded-full w-14 h-14 object-cover border border-gray-400 "
         />
         <div className="flex flex-col mt-2 text-sm">
-          <span className="font-semibold">dualipa</span>
-          <span className="text-gray-800">Dua 💋</span>
+          <span className="font-semibold">{user?.username}</span>
+          <span className="text-gray-800">{user?.desc}</span>
         </div>
       </div>
       <div className="mt-6 flex justify-between font-semibold">
