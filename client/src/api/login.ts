55ec552433d709e1b583b5102886c8fa9f2090dd
Saman Manesh <samanmanesh7@gmail.com>
2022-05-21 09:35:13 -0400

    about to start working on like

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -6,9 +6,6 @@ export interface UserLoginCredentials {
   password: string;
   email?: string;
 }
-interface Props {
-  userCredentials: UserLoginCredentials;
-}
 
 export const login = async (userCredentials: UserLoginCredentials): Promise<AxiosResponse<User>> => {
   return instance.post("auth/login", userCredentials);
