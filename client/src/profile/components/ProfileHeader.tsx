de6b56d091b639ddc05e177688dc519caef19ae7
Saman Manesh <samanmanesh7@gmail.com>
2022-06-01 12:15:08 -0400

    fixed the profile header

--- client/src/profile/components/ProfileHeader.tsx
+++ client/src/profile/components/ProfileHeader.tsx
@@ -26,7 +26,7 @@ const ProfileHeader = ({ user, userDetails, userStatus }: Props) => {
   return (
     <div className="flex justify-center">
       <div className="max-w-4xl flex-1 flex border-b border-gray-400 py-8">
-        <div className="px-4 py-2 mr-4 md:mx-16 flex-shrink-0 grid place-items-center">
+        <div className="px-4 py-2 mr-4 md:ml-12 md:mr-16 flex-shrink-0 grid place-items-center">
           <img
             src={
               user?.profilePicture
@@ -39,26 +39,26 @@ const ProfileHeader = ({ user, userDetails, userStatus }: Props) => {
         </div>
         <div className="flex-1">
           {/* Username Section */}
-          <div className="flex flex-col sm:flex-row  items-start mb-4">
+          <div className="flex flex-col sm:flex-row items-start mb-4">
             <h1
               title={user.username}
-              className="font-medium text-2xl overflow-hidden whitespace-nowrap text-ellipsis w-40 sm:w-56 cursor-default"
+              className="font-medium text-2xl overflow-hidden whitespace-nowrap text-ellipsis w-40 sm:w-52 cursor-default"
             >
               {user.username}
             </h1>
 
             {/* here for button we need to show it based on if its current user?, a random profile, or a profile we followed  */}
-            {userStatus.isFollowing ? (
+            {userStatus.isCurrentUser ? (
               <button className="w-full sm:w-max mt-2 sm:mt-0 sm:ml-2 border px-2 py-1 text-neutral-700 font-bold text-sm whitespace-nowrap rounded">
                 Edit Profile
               </button>
-            ) : userStatus.isRandomProfile ? (
-              <>
-                <button className="w-full sm:w-max my-2 sm:mx-2 sm:mt-0 sm:ml-2 border px-2 py-1 text-neutral-700 font-bold text-sm whitespace-nowrap rounded ">
+            ) : userStatus.isFollowing ? (
+              <div className="flex space-x-2 my-4 sm:my-0 ">
+                <button className="w-full sm:w-max sm:mx-2 sm:mt-0 sm:ml-2 border px-2 py-1 text-neutral-700 font-bold text-sm whitespace-nowrap rounded ">
                   Message
                 </button>
                 <UserRemoveIcon className="w-16 h-[1.8rem] rounded border p-0.5 cursor-pointer" />
-              </>
+              </div>
             ) : (
               <button className="w-full sm:w-max mt-2 sm:mt-0 sm:ml-2  px-2 py-1 text-white font-bold text-sm whitespace-nowrap rounded bg-blue-500">
                 Follow
