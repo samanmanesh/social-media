728f64c9e819ef4159ae9b2a82a975bce5c4bf51
Saman Manesh <samanmanesh7@gmail.com>
2022-06-15 10:31:23 -0400

    added see all for all friend suggestion features

--- client/src/friends/FriendsPage.tsx
+++ client/src/friends/FriendsPage.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { useAuth } from "auth";
+import React, { useEffect } from "react";
+import { getPeople, UserSuggestion } from "api";
+import { FriendSuggestion } from "./components/FriendSuggestion";
 
-type Props = {}
+type Props = {};
 
 const FriendsPage = (props: Props) => {
-  return (
-    <div>FriendsPage</div>
-  )
-}
+  const { user } = useAuth();
+  const [people, setPeople] = React.useState<UserSuggestion[]>([]);
 
-export default FriendsPage
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        if (user) {
+          const peopleList = await getPeople(user._id);
+          const excludeCurrentUser = peopleList.data.filter(
+            (person) => person._id !== user._id
+          );
+          setPeople(excludeCurrentUser);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPeople();
+  }, [user]);
+
+  return (
+    <div className="container grid max-w-5xl mt-20">
+      <h1 className="font-bold text-base p-3">Suggested</h1>
+      <div className="flex flex-col justify-between  h-full shadow rounded">
+        <div className="flex flex-col space-y-4 p-3">
+          {people.map((person) => (
+            <FriendSuggestion key={person._id} person={person} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FriendsPage;
